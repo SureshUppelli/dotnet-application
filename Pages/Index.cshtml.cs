90451549582d149bbc08bf2d59ac37794e3f82fd
SureshUppelli <45890987+SureshUppelli@users.noreply.github.com>
2023-06-02 00:45:23 +0530

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -11,10 +11,52 @@ namespace dotnet_application.Pages
         {
             _logger = logger;
         }
-
+        
         public void OnGet()
         {
 
         }
+        // vulneravle code
+        [HttpGet]
+public ActionResult DownloadFile(string fileName)
+{
+    var filePath = $"~/files/{fileName}";
+    var fullPath = Server.MapPath(filePath);
+    return File(fullPath, "application/octet-stream", fileName);
+}
+[HttpPost]
+public ActionResult ParseXML()
+{
+    var xmlInput = Request.InputStream;
+    var xmlDoc = new XmlDocument();
+    xmlDoc.Load(xmlInput);
+    // Process XML
+}
+var query = "SELECT * FROM Users WHERE Username = '" + userInput + "'";
+var command = new SqlCommand(query, connection);
+var json = "{ \"type\": \"MyApp.User\", \"data\": \"some malicious payload\" }";
+var user = JsonConvert.DeserializeObject(json);
+
+[HttpGet]
+public ActionResult ViewOrder(int orderId)
+{
+    Order order = dbContext.Orders.FirstOrDefault(o => o.Id == orderId);
+    return View(order);
+}
+
+var command = "ping " + ipAddress;
+var process = new Process
+{
+    StartInfo = new ProcessStartInfo
+    {
+        FileName = "cmd.exe",
+        Arguments = "/c " + command,
+        RedirectStandardOutput = true,
+        UseShellExecute = false,
+        CreateNoWindow = true
+    }
+};
+
+        //end of vulneralble code 
     }
 }
